7b0f1c69f38b74a4b4ae99e0a74880348c9fe729
宋成龙 <15246010266@163.com>
2018-01-26 09:46:46 +0800

    重新导入删除的文件

---
+++ gitTest/gitTest/Git.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest
+{
+    class Git
+    {
+        public int gitLength;
+        public string gitName;
+        public string temaName;
+    }
+    enum GitType
+    {
+        Nnormal,
+        Special
+    }
+}
---
+++ gitTest/gitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("git"); 
+            //添加一行注释 看看git会不会改变
+        }
+    }
+}
